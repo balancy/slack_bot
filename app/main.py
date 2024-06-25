0c2581d8744776111eff4fcdb3df41d799d7c068
balancy <balancy@gmail.com>
2024-06-25 23:56:49 +0200

    Add callback endpoint.

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ import logging
 from environs import Env
 from fastapi import BackgroundTasks, FastAPI, Request
 from pydantic import BaseModel
+from slack_sdk import WebClient
 
 from app.slack_commands import handle_command
 from app.slack_events import handle_event, verify_slack_request
@@ -21,6 +22,7 @@ env.read_env()
 
 app.state.bot_token = env.str("SLACK_BOT_TOKEN")
 app.state.signing_secret = env.str("SLACK_SIGNING_SECRET")
+
 
 class SlackEvent(BaseModel):
     type: str
@@ -56,7 +58,7 @@ async def slack_commands(
 
 @app.get("/slack/oauth/callback")
 async def oauth_callback(request: Request):
-    code = request.query_params.get('code')
+    code = request.query_params.get("code")
     if not code:
         return {"error": "No code provided"}
 
@@ -67,11 +69,12 @@ async def oauth_callback(request: Request):
             client_id=client_id,
             client_secret=client_secret,
             code=code,
-            redirect_uri="https://somepetprojects.ru/slack/oauth/callback"
+            redirect_uri="https://somepetprojects.ru/slack/oauth/callback",
         )
         return {"ok": True, "response": response}
     except SlackApiError as e:
         return {"error": str(e)}
+
 
 if __name__ == "__main__":
     logger.info("Starting server...")
